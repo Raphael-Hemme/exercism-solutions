f047a444d44446b99b805de2e6a4101727bbb76d
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2024-04-30 14:02:55 +0200

    clojure: Work on second task in log_levels

--- clojure/log_levels.clj
+++ clojure/log_levels.clj
@@ -7,10 +7,21 @@
   [s]
   (str/trim (str/replace s #"\[(.*?)\]:" "")))
 
+(defn message2
+  "Takes a string representing a log line
+   and returns its message with whitespace trimmed."
+  [s]
+  (-> (str/replace s #"\[(.*?)\]:" "")
+      (str/trim)))
+
 (defn log-level
   "Takes a string representing a log line
    and returns its level in lower-case."
-  [s])
+  [s]
+  (-> (str/split s #"]")
+      (first)
+      (str/replace ,,, "[" "")
+      (str/lower-case ,,,)))
 
 (defn reformat
   "Takes a string representing a log line and formats it
@@ -21,3 +32,8 @@
 
 (message "[ERROR]: Invalid operation")
 (message "[WARNING]:  Disk almost full\r\n")
+
+(message2 "[ERROR]: Invalid operation")
+(message "[WARNING]:  Disk almost full\r\n")
+
+(log-level "[WARNING]:  Disk almost full\r\n")
