aa8115ce496dbbcd9ff0f9f63582dc50a5207950
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2024-05-02 20:53:18 +0200

    clojure: Solve task 2 of squeaky_clean with help of tasxatzial's solution

--- clojure/squeaky_clean.clj
+++ clojure/squeaky_clean.clj
@@ -1,11 +1,25 @@
 (ns squeaky-clean
   (:require [clojure.string :as str]))
 
+;; (defn replaceControlChar
+;;   [c] 
+;;   ((if (Character/isISOControl c) "CTRL" c)))
+
+(defn replaceControlChar
+  [c]
+  ((if (Character/isISOControl c) "CTRL" c)))
+
 (defn clean
   "TODO: add docstring"
   [s]
-  (str/replace s " " "_"))
+  (-> s
+      (str/replace " " "_")
+      (str/replace #"\p{Cc}" "CTRL")))
+
 
 
 (clean "my   Id")
 (clean "my   Id  ")
+
+
+(clean "my\u007FId")
