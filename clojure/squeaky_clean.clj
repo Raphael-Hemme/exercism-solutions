9b5085739a48665a9a60635e4fa35643639ca09b
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2024-05-03 18:27:07 +0200

    clojure: add solution for task 4 of squeaky_clean

--- clojure/squeaky_clean.clj
+++ clojure/squeaky_clean.clj
@@ -7,20 +7,25 @@
       (map-indexed #(if (> %1 0) (str/capitalize %2) %2))
       (apply str)))
 
+(defn replace-non-letters
+  [s]
+  (str/replace s #"/[^\p{L}\-]/gu" ""))
+
 (defn clean
   "TODO: add docstring"
   [s]
   (-> s
       (str/replace " " "_")
       (str/replace #"\p{Cc}" "CTRL")
-      (kebab-to-camel-case ,,,)))
+      (kebab-to-camel-case)
+      (str/replace #"[^\p{L}\p{M}\-\_]" "")))
 
 
+;; Test cases
 
 (clean "my   Id")
 (clean "my   Id  ")
-
-
 (clean "my\u007FId")
-  
 (clean "nice-test-case")
+(clean "1😀2😀3😀")
+(clean "nice_test-case")
